84cfee42d4e045c0429fd668201b31538701453f
Pimashi Wickramarachchi <nipulee2022pi@gmail.com>
2026-03-13 22:54:53 +0530

    Create staging table for weather data in SQL Server
    This SQL script creates the staging table for weather data.

--- DataSources/source2_openmeteo/create_source_db.sql
+++ DataSources/source2_openmeteo/create_source_db.sql
@@ -1,1 +1,17 @@
 
+-- ============================================================
+-- Task 2 - Source Type 2: SQL Server Database
+-- Creates BerlinSBahn_Source and staging table
+-- ============================================================
+
+USE BerlinSBahn_Source;
+
+DROP TABLE IF EXISTS dbo.stg_weather_openmeteo;
+
+CREATE TABLE dbo.stg_weather_openmeteo (
+    timestamp         DATETIME      NOT NULL,
+    temperature_c     FLOAT         NOT NULL,
+    precipitation_mm  FLOAT         NOT NULL,
+    wind_speed_kmh    FLOAT         NOT NULL,
+    weather_condition VARCHAR(50)   NOT NULL
+);
